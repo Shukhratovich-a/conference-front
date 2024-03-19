4aac0e18f053af04fa990b96d1f41e376edcb1ab
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-19 12:07:20 +0500

    refactor: export created views

--- views/index.ts
+++ views/index.ts
@@ -5,3 +5,7 @@ export * from "./Participants/Participants.view";
 export * from "./Organizers/Organizers.view";
 export * from "./Speakers/Speakers.view";
 export * from "./Topics/Topics.view";
+
+export * from "./Profile/Profile.view";
+export * from "./Articles/Articles.view";
+export * from "./CreateArticle/CreateArticle.view";
