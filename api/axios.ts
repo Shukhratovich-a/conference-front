fb9cb9a93d9a6ee003cee1dc58b1427fdceaed7d
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-19 12:03:52 +0500

    refactor: create file upload

--- api/axios.ts
+++ api/axios.ts
@@ -6,4 +6,15 @@ const axiosInstance = axios.create({
   baseURL: DOMAIN,
 });
 
+interface IFile {
+  url: string;
+  name: string;
+}
+
+export const fileUpload = async (file: File) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  return axiosInstance.post<IFile>("/file/upload", formData, { headers: { "Content-Type": "multipart/form-data" } });
+};
+
 export default axiosInstance;
