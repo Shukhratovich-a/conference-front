d55ead5abcceb9bf7a539f273198e2bfb8c65005
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-19 12:13:45 +0500

    refactor: connect auth api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import { FC } from "react";
-import { GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
+
+import { IUser } from "@/types/user.type";
+
+import { getByToken } from "@/api/user.api";
 
 import { withLayout } from "@/layout/Layout";
 
@@ -7,13 +11,26 @@ const HomePage: FC<HomePageProps> = ({}) => {
   return <>Home</>;
 };
 
-export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
+export const getServerSideProps: GetServerSideProps<HomePageProps> = async ({ req: { cookies } }) => {
+  const token = cookies.token || null;
+  let user = null;
+
+  if (token) {
+    const { data } = await getByToken(token);
+    user = data;
+  }
+
   return {
-    props: {},
-    revalidate: 10,
+    props: {
+      token,
+      user,
+    },
   };
 };
 
 export default withLayout(HomePage);
 
-interface HomePageProps extends Record<string, unknown> {}
+interface HomePageProps extends Record<string, unknown> {
+  token: string | null;
+  user: IUser | null;
+}
