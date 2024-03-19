8aeaf91afa4fc89d7e8b5e2ea461e971f2f1fe2a
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-19 12:15:10 +0500

    refactor: edit header for auth

--- layout/Header/Header.component.tsx
+++ layout/Header/Header.component.tsx
@@ -1,88 +1,43 @@
-import { FC, useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { FC, useContext } from "react";
+import Link from "next/link";
 import cn from "classnames";
 
-import { IUser } from "@/types/user.type";
 import { HeaderProps } from "./Header.props";
 
-import { getAuthToken, logout } from "@/helpers/auth.helper";
+import { AuthContext } from "@/contexts/auth.context";
 
 import { Button } from "@/components";
 import { Navbar } from "../Navbar/Navbar.component";
 
 import styles from "./Header.module.scss";
-import { getByToken } from "@/api/user.api";
-import Link from "next/link";
 
 export const Header: FC<HeaderProps> = ({ className, ...props }) => {
-  const { pathname, replace } = useRouter();
-
-  const token = getAuthToken();
-
-  const [userState, setUserState] = useState<IUser | null>(null);
-  const [tokenState, setTokenState] = useState<string | null>(token as string);
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  useEffect(() => {
-    if (!token) {
-      setUserState(null);
-      setTokenState(null);
-      return;
-    }
-
-    setTokenState(token);
-
-    (async () => {
-      const { data } = await getByToken(token);
-
-      setUserState(data);
-    })();
-  }, [token]);
+  const { token, user, logout } = useContext(AuthContext);
 
   const handleLogout = () => {
-    setUserState(null);
-    setTokenState(null);
+    if (!logout) return;
 
-    if (pathname.startsWith("/profile")) replace("/auth/login");
     logout();
   };
-
-  if (!isMounted) {
-    return (
-      <header className={cn(styles.header, className)} {...props}>
-        <div className={cn(styles.top)}>
-          <div className={cn(styles.inner)}>
-            <div></div>
-
-            <div></div>
-          </div>
-        </div>
-
-        <div className={cn(styles.bottom)}>
-          <Navbar className={cn(styles.nav)} />
-        </div>
-      </header>
-    );
-  }
 
   return (
     <header className={cn(styles.header, className)} {...props}>
       <div className={cn(styles.top)}>
         <div className={cn(styles.inner)}>
-          <div></div>
+          <div className={cn(styles.logo)}>Logo</div>
 
-          <div>
-            {userState && tokenState ? (
+          <div className={cn(styles.content)}>Some info</div>
+
+          <div className={cn(styles.user)}>
+            {user && token ? (
               <div>
-                <Button className={cn(styles.button)} size="sm">
-                  <Link href={`/profile/${userState.id}`}>{`${userState.firstName} ${userState.lastName}`}</Link>
-                </Button>
+                <Link href={`/profile`}>
+                  <Button className={cn(styles.button)} size="sm" tabIndex={-1}>
+                    {`${user.firstName} ${user.lastName}`}
+                  </Button>
+                </Link>
                 <Button className={cn(styles.button)} size="sm" onClick={handleLogout}>
-                  Log out
+                  Logout
                 </Button>
               </div>
             ) : (
@@ -102,5 +57,3 @@ export const Header: FC<HeaderProps> = ({ className, ...props }) => {
     </header>
   );
 };
-
-export default Header;
