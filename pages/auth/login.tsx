7e6c7262ff7c18b17de3b19ca6d4c4c2a6bd2833
shukhratovich-a <shukhratovich138@gmail.com>
2024-04-04 14:37:51 +0500

    refactor: correct props

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,9 +1,30 @@
+import { FC } from "react";
+import { GetStaticProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import { IUser } from "@/types/user.type";
+
 import { withLayout } from "@/layout/Layout";
 
 import { LoginView } from "@/views";
 
-const LoginPage = () => {
+const LoginPage: FC<LoginPageProps> = () => {
   return <LoginView />;
 };
 
+export const getStaticProps: GetStaticProps<LoginPageProps> = async ({ locale }) => {
+  return {
+    props: {
+      token: null,
+      user: null,
+      ...(await serverSideTranslations(String(locale))),
+    },
+  };
+};
+
 export default withLayout(LoginPage);
+
+interface LoginPageProps extends Record<string, unknown> {
+  token: string | null;
+  user: IUser | null;
+}
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,9 +1,30 @@
+import { FC } from "react";
+import { GetStaticProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import { IUser } from "@/types/user.type";
+
 import { withLayout } from "@/layout/Layout";
 
 import { RegisterView } from "@/views";
 
-const RegisterPage = () => {
+const RegisterPage: FC<RegisterPageProps> = () => {
   return <RegisterView />;
 };
 
+export const getStaticProps: GetStaticProps<RegisterPageProps> = async ({ locale }) => {
+  return {
+    props: {
+      token: null,
+      user: null,
+      ...(await serverSideTranslations(String(locale))),
+    },
+  };
+};
+
 export default withLayout(RegisterPage);
+
+interface RegisterPageProps extends Record<string, unknown> {
+  token: string | null;
+  user: IUser | null;
+}
