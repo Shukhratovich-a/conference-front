ab113709084cd431fddcbfe2a226914262a52dda
shukhratovich-a <shukhratovich138@gmail.com>
2024-04-04 14:37:01 +0500

    refactor: connect helper and create translate with default key

--- layout/Sidebar/Sidebar.component.tsx
+++ layout/Sidebar/Sidebar.component.tsx
@@ -5,24 +5,27 @@ import cn from "classnames";
 
 import { SidebarProps } from "./Sidebar.props";
 
+import { sidebarMenu } from "@/helpers/sidebar.helper";
+
 import styles from "./Sidebar.module.scss";
 
 export const Sidebar: FC<SidebarProps> = ({ className, ...props }) => {
   const { t } = useTranslation();
 
+  const translate = (key: string, defaultText: string) => {
+    return t(key) === key || !!t(key) ? t(key) : defaultText;
+  };
+
   return (
     <nav className={cn(styles.nav, className)} {...props}>
       <ul className={cn(styles.list)}>
-        <li className={cn(styles.item)}>
-          <Link className={cn(styles.link)} href={"/sections"}>
-            {t("sections")}
-          </Link>
-        </li>
-        <li className={cn(styles.item)}>
-          <Link className={cn(styles.link)} href={"/contacts"}>
-            {t("contacts")}
-          </Link>
-        </li>
+        {sidebarMenu.map((item) => (
+          <li className={cn(styles.item)} key={item.id}>
+            <Link className={cn(styles.link)} href={item.route}>
+              {translate(item.translate, item.title)}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
