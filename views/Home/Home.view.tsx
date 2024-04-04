27e86d7151962d96ccd201d28cf623c31274549f
shukhratovich-a <shukhratovich138@gmail.com>
2024-04-04 14:39:35 +0500

    refactor: connect home page api

--- views/Home/Home.view.tsx
+++ views/Home/Home.view.tsx
@@ -3,20 +3,16 @@ import cn from "classnames";
 
 import { HomeViewProps } from "./Home.props";
 
+import { MainText } from "@/components";
+
 import styles from "./Home.module.scss";
 
-export const HomeView: FC<HomeViewProps> = ({ className, ...props }) => {
+export const HomeView: FC<HomeViewProps> = ({ className, homepage, sections, ...props }) => {
   return (
     <div className={cn(styles.view, className)} {...props}>
       <h2 className={cn(styles.heading)}>Home</h2>
 
-      <p>
-        Мирзо Улуғбек номидаги Ўзбекистон Миллий университети, Шароф Рашидов номидаги Самарқанд давлат университети, ЎзР
-        ФА Математика институти, Natural Science publication Ўзбекистон миллий университетининг 105 йиллиги, Мусо
-        ал-Хоразмийнинг 1240 йиллигига бағишланган навбатдаги VIII халқаро илмий “Амалий математика ва ахборот
-        технологияларнинг долзарб муаммолари” – Ал-Хоразмий 2023 халқаро илмий конференциясини 2023 йил 25–26 сентябр
-        кунлари Самарқанд давлат университетида ўтказади.
-      </p>
+      <MainText mainText={homepage.mainText} sections={sections} />
     </div>
   );
 };
