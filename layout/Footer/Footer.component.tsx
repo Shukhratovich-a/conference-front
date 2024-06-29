241a37d47d764409d26f8d301b7491ba9b6ad7aa
shukhratovich-a <shukhratovich138@gmail.com>
2024-06-30 00:13:27 +0500

    refactor: change translations

--- layout/Footer/Footer.component.tsx
+++ layout/Footer/Footer.component.tsx
@@ -1,19 +1,20 @@
 import { FC } from "react";
 import cn from "classnames";
+import { useTranslation } from "next-i18next";
 
 import { FooterProps } from "./Footer.props";
 
 import styles from "./Footer.module.scss";
 
 export const Footer: FC<FooterProps> = ({ className, ...props }) => {
+  const { t } = useTranslation();
+
   return (
     <footer className={cn(styles.footer, className)} {...props}>
       <div className={cn(styles.inner)}>
-        <span className={cn(styles.left)}>
-          The 9th International Conference on Control and Optimization with Industrial Applications
-        </span>
+        <span className={cn(styles.left)}>{t("footer")}</span>
 
-        <span className={cn(styles.right)}>Nuu</span>
+        {/* <span className={cn(styles.right)}>Nuu</span> */}
       </div>
     </footer>
   );
