90085199f941b92f6fd6afdaf076de739ceb2973
shukhratovich-a <shukhratovich138@gmail.com>
2024-07-28 17:37:29 +0500

    refactor: export imported icons

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,1 +1,3 @@
 export { default as IconArrowBottom } from "./arrow-bottom.svg";
+export { default as IconArrowLeft } from "./arrow-left.svg";
+export { default as IconArrowRight } from "./arrow-right.svg";
