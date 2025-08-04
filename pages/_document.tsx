11ecf9d911013a384bc10975f6b1e0f7a76d06e9
Shukhratovich-a <94617261+Shukhratovich-a@users.noreply.github.com>
2025-08-04 13:36:32 +0500

    refactor: correct page direction

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,20 @@
 import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from "next/document";
 
+import i18nextConfig from "../../next-i18next.config.js";
+
 class MyDocument extends Document {
   static getInitialProps = async (ctx: DocumentContext): Promise<DocumentInitialProps> => {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   };
 
+  onst currentLocale = this.props.__NEXT_DATA__.locale || i18nextConfig.i18n.defaultLocale;
+
+  const isRtl = currentLocale === "ar" ? "content-rtl" : "";
+
   render(): JSX.Element {
     return (
-      <Html lang="en">
+      <Html dir={currentLocale === "ar" ? "rtl" : "ltr"} lang={currentLocale}>
         <Head />
 
         <body>
